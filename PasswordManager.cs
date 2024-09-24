19b29fd4642c0f2fd29d267ae6b94fd9ec7c288d
BarkevReisig <barkevreisig@gmail.com>
2024-09-24 00:43:15 -0400

    Renamed file

---
+++ PasswordManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static InputHandling;
+using static UserInterface;
+
+// On program start...
+PrintWelcome();
+// ...
+
+// Main loop...
+while (true)
+{
+    string input = Console.ReadLine() ?? throw new NullReferenceException();
+    input = input.ToLower();
+
+    if (input is "q" or "quit")
+    {
+        return;
+    }
+
+    ProcessInput(input);
+}
